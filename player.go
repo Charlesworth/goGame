3d8b1496529c1caf73a9bb3742ba40f8b5717e27
charlesworth <cochrane26@gmail.com>
2016-07-16 15:44:32 +0700

    player can no longer leave the screen

--- player.go
+++ player.go
@@ -40,42 +40,33 @@ func (player *player) calculateMovement(events *Events) {
 		speed = player.speedOnAxis
 	}
 
+	xMove, yMove := 0, 0
+
 	if events.left {
-		player.rect.X -= speed
-		player.weapon.rect.X -= speed
+		xMove -= speed
 	} else if events.right {
-		player.rect.X += speed
-		player.weapon.rect.X += speed
+		xMove += speed
 	}
 
 	if events.up {
-		player.rect.Y -= speed
-		player.weapon.rect.Y -= speed
+		yMove -= speed
 	} else if events.down {
-		player.rect.Y += speed
-		player.weapon.rect.Y += speed
+		yMove += speed
+	}
+
+	if (player.rect.X+xMove) > 0 &&
+		(player.rect.X+player.rect.W+xMove) < winWidth {
+		player.rect.X += xMove
+		player.weapon.rect.X += xMove
+	}
+
+	if (player.rect.Y+yMove) > 0 &&
+		(player.rect.Y+player.rect.H+yMove) < winHeight {
+		player.rect.Y += yMove
+		player.weapon.rect.Y += yMove
 	}
 
 	player.weapon.calculateMovement()
-	player.keepInWindow()
-}
-
-func (player *player) keepInWindow() {
-	if (player.rect.Y + player.rect.H) > winHeight {
-		player.rect.Y = winHeight - player.rect.H
-	}
-
-	if player.rect.Y < 0 {
-		player.rect.Y = 0
-	}
-
-	if (player.rect.X + player.rect.W) > winWidth {
-		player.rect.X = winWidth - player.rect.W
-	}
-
-	if player.rect.X < 0 {
-		player.rect.X = 0
-	}
 }
 
 type spinWeapon struct {
